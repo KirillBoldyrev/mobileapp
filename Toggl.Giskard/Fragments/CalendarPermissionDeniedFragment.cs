6db98c19c6b6748e9424094800e9dc2be5351151
Juan Laube <juanlaube@icloud.com>
2019-02-26 15:47:52 -0300

    :art: Implement layout

--- Toggl.Giskard/Fragments/CalendarPermissionDeniedFragment.cs
+++ Toggl.Giskard/Fragments/CalendarPermissionDeniedFragment.cs
@@ -1,6 +1,8 @@
 ﻿using Android.OS;
 using Android.Views;
 using Toggl.Foundation.MvvmCross.ViewModels.Calendar;
+using Toggl.Giskard.Extensions.Reactive;
+using Toggl.Multivac.Extensions;
 
 namespace Toggl.Giskard.Fragments
 {
@@ -9,6 +11,16 @@ namespace Toggl.Giskard.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.CalendarPermissionDeniedFragment, container, false);
+            InitializeViews(view);
+
+            continueButton.Rx().Tap()
+                .Subscribe(ViewModel.Close.Inputs)
+                .DisposedBy(DisposeBag);
+
+            allowAccessButton.Rx().Tap()
+                .Subscribe(ViewModel.EnableAccess.Inputs)
+                .DisposedBy(DisposeBag);
+
             return view;
         }
     }
